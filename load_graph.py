25e006f7b45156ec770acbc55e81519ac6cd082a
khizar-z <khiz.zaman@gmail.com>
2026-03-25 23:41:22 -0400

    Load categories

--- load_graph.py
+++ load_graph.py
@@ -1,33 +1,43 @@
 from __future__ import annotations
+import sys
 import csv
 
 from graph import Graph
 
+csv.field_size_limit(sys.maxsize)  # We are working with ridiculous files.
 
-def load_graph(hyperlinks_file: str, page_name_file: str) -> Graph:
+
+def load_graph(hyperlinks_file: str, page_name_file: str, categories_file: str) -> tuple[Graph, dict[int, set[str]]]:
     """Return a Wikipedia graph corresponding to the given datasets.
     """
     graph = Graph()
-    articles = {}
+    articles: dict[int, str] = {}  # { id: name }
 
-    with open(page_name_file) as csvfile:
-        new_page_name = csv.reader(csvfile)
+    with open(page_name_file) as file:
+        new_page_name = csv.reader(file)
         for row in new_page_name:
             cleaned_line = row[0].split(' ', maxsplit=1)
-            articles[cleaned_line[0]] = cleaned_line[1]
+            articles[int(cleaned_line[0])] = cleaned_line[1]
             graph.add_vertex(cleaned_line[1], int(cleaned_line[0]))
             print(f"Added article ID {cleaned_line[0]} for {cleaned_line[1]}.")
 
-    with open(hyperlinks_file) as csvfile:
-        new_hyperlinks_file = csv.reader(csvfile)
+    with open(hyperlinks_file) as file:
+        new_hyperlinks_file = csv.reader(file)
         for row in new_hyperlinks_file:
             cleaned_line = row[0].split(' ', maxsplit=1)
             article1, article2 = cleaned_line[0], cleaned_line[1]
             if article1 != article2:
-                graph.add_forward_edge(articles[cleaned_line[0]], articles[cleaned_line[1]])
+                graph.add_forward_edge(articles[int(cleaned_line[0])], articles[int(cleaned_line[1])])
                 print(f"Added edge between {article1} and {article2}.")
 
-    return graph
+    categories: dict[int, set[str]] = {aid: set() for aid in articles}  # { id: set(categories) }
 
-
-load_graph('wiki-topcats.txt', 'wiki-topcats-page-names.txt')
+    with open(categories_file) as file:
+        for row in csv.reader(file):
+            parts = row[0].split()
+            category_name = parts[0].rstrip(';')
+            for aid in parts[1:]:
+                if int(aid) in articles:
+                    categories[int(aid)].add(category_name)
+                    print(f"Added {category_name} to article ID {aid}.")
+    return graph, categories
--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from load_graph import load_graph
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def init(start, end, graph):
+    """
+    Docstring unimplemented
+    """
+    pass
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    graph, categories = load_graph(
+        'wiki-topcats.txt',
+        'wiki-topcats-page-names.txt',
+        'wiki-topcats-categories.txt'
+    )
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
-# hi
