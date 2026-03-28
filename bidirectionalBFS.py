7636d0b53270b2f8b59cb8ab285252c160e7c4e4
safid <safid2007@gmail.com>
2026-03-28 19:44:13 -0400

    fixed bidirectionalBFS but the documentation no bueno

--- bidirectionalBFS.py
+++ bidirectionalBFS.py
@@ -187,14 +187,14 @@ import graph
 
 
 def bi_bfs_all(graph1: graph.Graph, start: str, end: str, max_path: int) -> list[list[str]] | bool:
-    """
+    """Returns the least amount of steps required to get from start to finish.
 
     Preconditions:
         - start in graph1.get_all_vertices()
         - end in graph1.get_all_vertices()
     """
-    start_v = graph1.get_vertex(start)
-    end_v = graph1.get_vertex(end)
+    start_v = graph1.get_vertex_by_name(start)
+    end_v = graph1.get_vertex_by_name(end)
     all_path: list[list[str]] = []
     size_of_shortest_path = math.inf
 
@@ -202,54 +202,62 @@ def bi_bfs_all(graph1: graph.Graph, start: str, end: str, max_path: int) -> list
         return False
 
     # Sets up variables for the forward direction
-    queue = deque([start_v])
-    visited: set = {start_v}
-    parent: dict[str: str | None] = {start_v: None}
+    queue = deque([start])
+    visited: set[str] = {start}
+    parent: dict[str, str | None] = {start: None}
 
     # Sets up variables for the reverse direction
-    queue_rev = deque([end_v])
-    visited_rev: set = {end_v}
-    parent_rev: dict = {end_v: None}
+    queue_rev = deque([end])
+    visited_rev: set[str] = {end}
+    parent_rev: dict[str, str | None] = {end: None}
 
-    while (queue and queue_rev) and len(all_path) < max_path:
-        node = queue.popleft()
+    while queue and queue_rev:
+        found_this_round = False
 
-        for neighbor in node.forward_links:
-            if neighbor not in visited:
-                visited.add(neighbor)
-                parent[neighbor] = node
-                queue.append(neighbor)
+        # Expands a full layer of the tree
+        level_size = len(queue)
+        for _ in range(level_size):
+            node = queue.popleft()
 
-                if neighbor in visited_rev:
-                    path = _build_path(neighbor, parent, parent_rev)
-                    if len(path) < size_of_shortest_path:  #make another since its the same below too
-                        all_path.append(path)
-                        size_of_shortest_path = len(path)
-                        _check_current_list(all_path, size_of_shortest_path)  # will check if there are any path bigger than size of shortest
-                    elif len(path) == size_of_shortest_path:
-                        all_path.append(path)
-                        _check_current_list(all_path, size_of_shortest_path)
+            for neighbor in graph1.get_vertex_by_name(node).forward_links:
+                if neighbor.article_name not in visited:
+                    visited.add(neighbor.article_name)
+                    parent[neighbor.article_name] = node
+                    queue.append(neighbor.article_name)
 
-        node_rev = queue_rev.popleft()
+                    if neighbor.article_name in visited_rev:
+                        path = _build_path(neighbor.article_name, parent, parent_rev)
 
-        for neighbor in node_rev.reverse_links:
-            if neighbor not in visited_rev:
-                visited_rev.add(neighbor)
-                parent_rev[neighbor] = node_rev
-                queue_rev.append(neighbor)
+                        if len(path) < size_of_shortest_path:
+                            size_of_shortest_path = len(path)
+                            all_path = [path]
+                        elif len(path) == size_of_shortest_path and path not in all_path:
+                            all_path.append(path)
+                        found_this_round = True
 
-                if neighbor in visited:
-                    path = _build_path(neighbor, parent, parent_rev)
-                    if len(path) < size_of_shortest_path:
-                        all_path.append(path)
-                        size_of_shortest_path = len(path)
-                        _check_current_list(all_path, size_of_shortest_path)  # will check if there are any path bigger than size of shortest
-                    elif len(path) == size_of_shortest_path:
-                        all_path.append(path)
-                        _check_current_list(all_path, size_of_shortest_path)
+        # Expands a full layer of the tree in the reverse direction
+        backward_layer_size = len(queue_rev)
+        for _ in range(backward_layer_size):
+            node_rev = queue_rev.popleft()
 
-    if all_path:
-        return all_path, len(all_path)
+            for neighbor in graph1.get_vertex_by_name(node_rev).reverse_links:
+                if neighbor.article_name not in visited_rev:
+                    visited_rev.add(neighbor.article_name)
+                    parent_rev[neighbor.article_name] = node_rev
+                    queue_rev.append(neighbor.article_name)
+
+                    if neighbor.article_name in visited:
+                        path = _build_path(neighbor.article_name, parent, parent_rev)
+
+                        if len(path) < size_of_shortest_path:
+                            size_of_shortest_path = len(path)
+                            all_path = [path]
+                        elif len(path) == size_of_shortest_path and path not in all_path:
+                            all_path.append(path)
+                        found_this_round = True
+
+        if found_this_round:
+            return all_path[:max_path], len(all_path[:max_path])
     return False
 
 
@@ -258,7 +266,7 @@ def _build_path(meeting_node, parent, parent_rev):
     node = meeting_node
 
     while node is not None:
-        path_forward.append(node.article_name)
+        path_forward.append(node)
         node = parent[node]
 
     path_forward.reverse()
@@ -267,16 +275,10 @@ def _build_path(meeting_node, parent, parent_rev):
     node = parent_rev[meeting_node]
 
     while node is not None:
-        path_backward.append(node.article_name)
+        path_backward.append(node)
         node = parent_rev[node]
 
     return path_forward + path_backward
-
-
-def _check_current_list(list_of_path, size) -> None:
-    for path in list_of_path:
-        if len(path) > size:
-            list_of_path.remove(path)
 
 
 """ WONT WORK
@@ -290,6 +292,16 @@ astar(graph, "Isotopes of terbium", "The Possibility of Hope", categories)
 """
 
 
-g = load_graph.load_graph('wiki-topcats.txt', 'wiki-topcats-page-names.txt')
+g = load_graph.load_graph('wiki-topcats.txt', 'wiki-topcats-page-names.txt', 'wiki-topcats-categories.txt')
+
+bi_bfs_all(g[0], "Live @ Slim's / Turbulence Chest", "Fujiwara", 5)
 
 print("end")
+
+
+
+#  bi_bfs_all(g[0], "Isotopes of terbium", "The Possibility of Hope", 5) -> 4
+#  bi_bfs_all(g[0], "Chiasmal syndrome", "Mrio Silva (football player)", 5) -> 1
+#  bi_bfs_all(g[0], "List of vegetable oils", "Tenwa Station", 5) -> 2
+#  bi_bfs_all(g[0], "List of vegetable oils", "Mini-Mag Orion", 5) -> 1
+#  bi_bfs_all(g[0], "Live @ Slim's / Turbulence Chest", "J. Michel Fournier", 5) -> 1
